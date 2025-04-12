9c9b6920ce1ab201d7ea3b2f578a6b26a689dfcd
Your Name <you@example.com>
2025-04-12 12:57:12 +0100

    add fuling logic

--- Super Mario/script.js
+++ Super Mario/script.js
@@ -2,6 +2,7 @@ const back = document.getElementById('background');
 const mario = document.getElementById('player');
 const marioim = document.getElementById('player-img');
 const coinEl = document.getElementById('coin');
+const over = document.getElementById('gameover')
 
 class Background {
     constructor(element) {
@@ -42,49 +43,49 @@ class Player {
     }
 
     moveLeft(deltaTime) {
-        
+
         this.moveright = false;
         if (!this.isJumping) {
-                  this.positionY = 395;
-        this.marioim.style.top = '0';
+            this.positionY = 395;
+            this.marioim.style.top = '0';
         }
-  
+
         if (this.positionX > 200) {
             this.positionX -= this.speed;
         } else if (this.game.background.positionX > 0) {
             this.game.background.positionX -= this.speed;
         }
         if (!this.isJumping) {
-            this.frameTimer+=deltaTime
+            this.frameTimer += deltaTime
             if (this.frameTimer >= this.frameInterval) {
                 this.frameTimer = 0;
-            if (this.frameX < 450) {
-                this.frameX =   850
-            }else{
-                this.frameX -= 150;
-            } 
+                if (this.frameX < 450) {
+                    this.frameX = 850
+                } else {
+                    this.frameX -= 150;
+                }
+            }
+            this.marioim.style.left = `-${this.frameX}%`;
         }
-            this.marioim.style.left = `-${this.frameX}%`; 
-        }
-        
+
     }
 
     moveRight(deltaTime) {
-     
-        
+
+
         this.moveright = true;
-    
+
         if (!this.isJumping) {
             this.positionY = 390;
             this.marioim.style.top = '-650%';
         }
-    
+
         if (this.positionX < 600) {
             this.positionX += this.speed;
         } else if (this.game.background.positionX < this.game.background.maxScroll) {
             this.game.background.positionX += this.speed;
         }
-    
+
         if (!this.isJumping) {
             this.frameTimer += deltaTime;
             if (this.frameTimer >= this.frameInterval) {
@@ -98,7 +99,7 @@ class Player {
             this.marioim.style.left = `-${this.frameX}%`;
         }
     }
-    
+
 
     jump() {
         if (!this.isJumping) {
@@ -113,73 +114,45 @@ class Player {
             }
         }
     }
-    
+
 
     applyGravity() {
         if (this.isJumping) {
             this.velocityY += this.gravity;
             this.positionY += this.velocityY;
-    
+
             if (this.moveright) {
-                this.marioim.style.top = '-650%'; 
-                this.marioim.style.left = '-720%'; 
+                this.marioim.style.top = '-650%';
+                this.marioim.style.left = '-720%';
             } else {
-                this.marioim.style.top = '0';      
-                this.marioim.style.left = '-150%'; 
+                this.marioim.style.top = '0';
+                this.marioim.style.left = '-150%';
             }
-    
+
             if (this.positionY >= 395) {
                 if (!this.moveright) {
-                      this.positionY = 395;
-                }else{
+                    this.positionY = 395;
+                } else {
                     this.positionY = 390;
                 }
-              
+
                 this.isJumping = false;
                 this.velocityY = 0;
                 this.marioim.style.left = this.moveright ? '0' : '-850%';
             }
         }
     }
-    
-    
+
+
 
     draw() {
-        this.element.style.left = `${this.positionX}px`;
+        if (!this.game.fulling) {
+            this.element.style.left = `${this.positionX}px`;
+        }
+
         this.element.style.top = `${this.positionY}px`;
-    }
-}
 
-class Coin {
-    constructor(element, game) {
-        this.element = element;
-        this.game = game;
-        this.collected = false;
-        this.positionX = 1000;
-        this.positionY = 395;
-    }
 
-    checkCollision() {
-        const player = this.game.player;
-        const distance = Math.abs((this.positionX - this.game.background.positionX) - player.positionX);
-        if (distance < 40 && !this.collected) {
-            this.collect();
-        }
-    }
-
-    collect() {
-        this.collected = true;
-        this.element.style.display = "none";
-        this.game.score++;
-        document.getElementById("score").textContent = `Score: ${this.game.score}`;
-    }
-
-    draw() {
-        if (!this.collected) {
-            const offset = this.positionX - this.game.background.positionX;
-            this.element.style.left = `${offset}px`;
-            this.element.style.top = `${this.positionY}px`;
-        }
     }
 }
 
@@ -196,10 +169,41 @@ class Input {
             if (['ArrowLeft', 'ArrowRight', ' ', 'p'].includes(e.key)) {
                 this.keys.splice(this.keys.indexOf(e.key), 1);
                 if (this.game.player.moveright) {
-                    this.game.player.marioim.style.left = '0' 
-                   
-                }else{
-                    this.game.player.marioim.style.left= '-850%';
+                    this.game.player.marioim.style.left = '0'
+
+                } else {
+                    this.game.player.marioim.style.left = '-850%';
+                }
+            }
+        });
+    }
+}
+class Map {
+    constructor(game, background, player) {
+        this.game = game;
+        this.background = background;
+        this.player = player;
+        this.holes = [
+            { startX: 2290, endX: 2330, yPosition: 500 },
+            { startX: 2850, endX: 2950, yPosition: 500 },
+            { startX: 5090, endX: 5150, yPosition: 1000 },
+        ];
+    }
+
+    update() {
+        this.holes.forEach(hole => {
+
+            if (this.player.positionX + this.background.positionX > hole.startX &&
+                this.player.positionX + this.background.positionX < hole.endX &&
+                !this.player.isJumping) {
+
+
+                if (this.player.positionY < hole.yPosition) {
+                    this.player.positionY += this.player.speed;
+                    this.player.marioim.style.left = 0;
+                    this.player.marioim.style.top = 0;
+                    this.game.fulling = true;
+
                 }
             }
         });
@@ -211,8 +215,10 @@ class Game {
         this.background = new Background(back);
         this.player = new Player(mario, this, marioim);
         this.input = new Input(this);
-        this.coin = new Coin(coinEl, this);
+        this.map = new Map(this, this.background, this.player)
         this.score = 0;
+        this.fulling = false;
+        this.gameOver=false;
     }
 
     updateInput(deltaTime) {
@@ -220,7 +226,7 @@ class Game {
         if (keys.includes(' ')) this.player.jump(deltaTime);
         if (keys.includes('ArrowLeft')) this.player.moveLeft(deltaTime);
         if (keys.includes('ArrowRight')) this.player.moveRight(deltaTime);
-        
+
     }
 
     draw(deltaTime) {
@@ -228,18 +234,25 @@ class Game {
         this.background.draw();
         this.player.applyGravity(deltaTime);
         this.player.draw();
-        this.coin.checkCollision();
-        this.coin.draw();
+        this.map.update();
+        if (this.player.positionY>430) {
+            this.gameOver=true
+        }
     }
 }
 
 const game = new Game();
-let lastTime =0;
+let lastTime = 0;
 function animation(timeStamp) {
     const deltaTime = timeStamp - lastTime;
     lastTime = timeStamp;
     game.draw(deltaTime);
-    requestAnimationFrame(animation);
+    if (!game.gameOver) {
+        requestAnimationFrame(animation);
+    }else{
+        over.style.display='block'
+    }
+    
 }
 
 animation(0);
