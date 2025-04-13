20f42ab2bf766aaedbc183e40effa1da18e11279
Your Name <you@example.com>
2025-04-13 18:01:19 +0100

    fix the ground

--- Super Mario/script.js
+++ Super Mario/script.js
@@ -27,32 +27,34 @@ class Background {
 class Player {
     constructor(element, game, marioim) {
         this.game = game;
-        this.width=50;
-        this.height=70;
+        this.width = 50;
+        this.height = 70;
         this.element = element;
         this.marioim = marioim;
         this.positionX = 300;
         this.positionY = 390;
         this.speed = 5;
-        this.jumpSpeed = 10;
+        this.jumpSpeed = 13;
         this.gravity = 0.5;
         this.frameTimer = 0;
         this.frameInterval = 100;
         this.frameX = 0;
-        this.onground=true;
+        this.ground = 390;
         this.isJumping = false;
         this.velocityY = 0;
         this.moveright = true;
+        this.fulling = false
+
     }
 
     moveLeft(deltaTime) {
 
         this.moveright = false;
         if (!this.isJumping) {
-            if (this.onground) {
-                 this.positionY = 395;
-            }
-           
+
+            this.positionY = this.ground;
+
+
             this.marioim.style.top = '0';
         }
 
@@ -82,9 +84,9 @@ class Player {
         this.moveright = true;
 
         if (!this.isJumping) {
-            if (this.onground) {
-                this.positionY = 390;
-           }
+
+            this.positionY = this.ground;
+
             this.marioim.style.top = '-650%';
         }
 
@@ -110,11 +112,13 @@ class Player {
 
 
     jump() {
+
         if (!this.isJumping) {
-            console.log("Jump triggered!");
+
             this.isJumping = true;
             this.velocityY = -this.jumpSpeed;
             this.positionY += this.velocityY;
+            console.log(this.velocityY, this.ground)
             if (this.moveright) {
                 this.marioim.style.left = '-720%'
             } else {
@@ -125,7 +129,7 @@ class Player {
 
 
     applyGravity() {
-        if (this.isJumping) {
+        if (this.isJumping || this.fulling) {
             this.velocityY += this.gravity;
             this.positionY += this.velocityY;
 
@@ -137,14 +141,15 @@ class Player {
                 this.marioim.style.left = '-150%';
             }
 
-            if (this.positionY >= 395) {
+            if (this.positionY >= this.ground) {
                 if (!this.moveright) {
-                    this.positionY = 395;
+                    this.positionY = this.ground;
                 } else {
-                    this.positionY = 390;
+                    this.positionY = this.ground;
                 }
 
                 this.isJumping = false;
+                this.fulling = false
                 this.velocityY = 0;
                 this.marioim.style.left = this.moveright ? '0' : '-850%';
             }
@@ -154,6 +159,10 @@ class Player {
 
 
     draw() {
+        if (this.positionY + 5 <= this.ground) {
+            this.fulling = true
+
+        }
         if (!this.game.fulling) {
             this.element.style.left = `${this.positionX}px`;
         }
@@ -191,66 +200,137 @@ class Map {
         this.game = game;
         this.background = background;
         this.player = player;
+
         this.holes = [
-            { startX: 2290, endX: 2330, yPosition: 500 },
+            { startX: 2290, endX: 2385, yPosition: 500 },
             { startX: 2850, endX: 2950, yPosition: 500 },
-            { startX: 5090, endX: 5150, yPosition: 1000 },
+            { startX: 5100, endX: 5190, yPosition: 500 },
         ];
+
         this.bloks = [
-            { startX: 900, endX: 990, startY: 390, endY: 330, box: false },
-            { startX: 1235, endX: 1325, startY: 390, endY: 310, box: false },
-            { startX: 1500, endX: 1590, startY: 390, endY: 340, box: false },
-            { startX: 1870, endX: 1940, startY: 390, endY: 340, box: false },
-            { startX: 4900, endX: 4990, startY: 390, endY: 365, box: false },
-        ]
+           /*onpop1*/ { startX: 930, endX: 990, startY: 395, endY: 395, box: false },
+            ///*onpop2*/  { startX: 1280, endX: 1325, startY: 390, endY: 310, box: false },
+            // /*onpop3*/ { startX: 1545, endX: 1590, startY: 390, endY: 340, box: false },
+            // /*onpop4*/ { startX: 1915, endX: 1940, startY: 390, endY: 340, box: false },
+            //  { startX: 4900, endX: 4990, startY: 390, endY: 365, box: false },
+            //  /*1*/ { startX: 545, endX: 560, startY: 325, endY: 302, box: true },
+            // /*2*/ { startX: 650, endX: 759, startY: 285, endY: 210, box: true },
+            // /*tall1*/{ startX: 680, endX: 840, startY: 325, endY: 302, box: true },
+        ];
+    }
+
+    getPlayerHitbox() {
+        return {
+            top: this.player.positionY,
+            bottom: this.player.positionY + this.player.height,
+            left: this.player.positionX + this.background.positionX,
+            right: this.player.positionX + this.background.positionX + this.player.width,
+        };
     }
 
     update() {
+        const playerBox = this.getPlayerHitbox();
+        let onBlock = false;
+
         this.holes.forEach(hole => {
-
-            if (this.player.positionX + this.background.positionX > hole.startX &&
-                this.player.positionX + this.background.positionX < hole.endX &&
-                !this.player.isJumping) {
-
-
+            if (
+                playerBox.left > hole.startX &&
+                playerBox.right < hole.endX &&
+                !this.player.isJumping
+            ) {
                 if (this.player.positionY < hole.yPosition) {
                     this.player.positionY += this.player.speed;
-                    this.player.marioim.style.left = 0;
-                    this.player.marioim.style.top = 0;
+                    this.player.marioim.style.left = '0';
+                    this.player.marioim.style.top = '0';
                     this.game.fulling = true;
-
                 }
             }
         });
-        this.player.onground = false;
 
         this.bloks.forEach(blok => {
+            const blokBox = {
+                top: blok.startY,
+                bottom: blok.endY,
+                left: blok.startX,
+                right: blok.endX
+            };
+
+            /* if (
+                 playerBox.top <= blokBox.bottom &&
+                 playerBox.top >= blokBox.bottom - 10 &&
+                 playerBox.right > blokBox.left &&
+                 playerBox.left < blokBox.right &&
+                 this.player.velocityY < 0 &&
+                 blok.box
+             ) {
+                 console.log("Hit box block!");
+                 blok.box = false;
+                 
+             }*/
+
+
             if (
-                this.player.positionX + this.background.positionX > blok.startX &&
-                this.player.positionX + this.background.positionX < blok.endX &&
-                this.player.positionY > blok.endY
+                this.player.moveright &&
+                playerBox.bottom <= blokBox.top &&
+                playerBox.right > blokBox.left &&
+                playerBox.left < blokBox.right
+
             ) {
-                if (this.game.player.moveright) {
-                    this.player.positionX = blok.startX - this.background.positionX;
-                } else {
-                    this.player.positionX = blok.endX - this.background.positionX;
+                this.player.ground = blokBox.top - this.player.height;
+                onBlock = true;
+                this.fulling = false;
+            }
+            console.log(playerBox.bottom <= blokBox.top, playerBox.right > blokBox.left, playerBox.left < blokBox.right)
+            if (
+                !this.player.moveright &&
+                playerBox.bottom <= blokBox.top &&
+                playerBox.right > blokBox.left &&
+                playerBox.left < blokBox.right
+
+            ) {
+                this.player.ground = blokBox.top - this.player.height;
+                onBlock = true;
+                this.fulling = false;
+            }
+
+
+            if (
+                playerBox.bottom > blokBox.top &&
+                playerBox.top < blokBox.bottom &&
+                playerBox.right > blokBox.left &&
+                playerBox.left < blokBox.right
+
+            ) {
+                if (this.player.moveright) {
+                    this.player.positionX = blokBox.left - this.background.positionX - this.player.width;
                 }
             }
-        
+
             if (
-                this.player.positionY + this.player.height <= blok.startY &&
-                this.player.positionX + this.background.positionX + this.player.width > blok.startX &&
-                this.player.positionX + this.background.positionX < blok.endX
+                playerBox.bottom > blokBox.top &&
+                playerBox.top - 5 < blokBox.bottom &&
+                playerBox.right > blokBox.left &&
+                playerBox.left < blokBox.right
+
             ) {
-                this.player.onground = true;
-                this.player.positionY = blok.startY - this.player.height;
-                this.player.velocityY = 0;
-                this.player.isJumping = false;
+                if (!this.player.moveright) {
+                    this.player.positionX = blokBox.right - this.background.positionX;
+                }
             }
         });
         
+
+        if (!onBlock) {
+
+            if (!this.player.moveright&&!this.player.isJumping) {
+                this.player.positionY = this.player.ground + 5
+            }
+            this.player.ground = 390;
+
+        }
     }
 }
+
 class Game {
     constructor() {
         this.background = new Background(back);
@@ -273,8 +353,9 @@ class Game {
     draw(deltaTime) {
         this.updateInput(deltaTime);
         this.background.draw();
-        this.player.applyGravity(deltaTime);
         this.player.draw();
+        this.player.applyGravity(deltaTime);
+
         this.map.update();
         if (this.player.positionY > 430) {
             this.gameOver = true
